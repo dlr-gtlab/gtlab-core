6d3af8c44247f528f60b5089c47760200a0249a7
Schmeink <Jens.Schmeink@dlr.de>
2020-02-04 17:26:37 +0100

    add missing path to qt lib

--- tests/build/build-512.bat
+++ tests/build/build-512.bat
@@ -6,6 +6,9 @@ set LIB=%LIB%;C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\
 set LIBPATH=%LIBPATH%;C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Tools\MSVC\14.10.25017\lib\x64;C:\Program Files (x86)\Windows Kits\10\lib\10.0.14393.0\ucrt\x64;C:\Program Files (x86)\Windows Kits\10\lib\10.0.14393.0\um\x64;
 set INCLUDE=%INCLUDE%;C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Tools\MSVC\14.10.25017\include;C:\Program Files (x86)\Windows Kits\10\include\10.0.14393.0\ucrt;C:\Program Files (x86)\Windows Kits\10\include\10.0.14393.0\shared;C:\Program Files (x86)\Windows Kits\10\include\10.0.14393.0\um;C:\Program Files (x86)\Windows Kits\10\include\10.0.14393.0\winrt;
 
+set LIB=%LIB%;C:\Qt\5.12.6\msvc2017_64\lib
+set LIBPATH=%LIBPATH%;C:\Qt\5.12.6\msvc2017_64\lib
+set INCLUDE=%INCLUDE%;C:\Qt\5.12.6\msvc2017_64\include;C:\Qt\5.12.6\msvc2017_64\include\QtTest
 
 C:\\Qt\\5.12.6\\msvc2017_64\\bin\\qmake.exe gtlab.pro -spec win32-msvc "CONFIG+=debug"
 C:\Qt\Tools\QtCreator\bin\jom.exe -f Makefile qmake_all
