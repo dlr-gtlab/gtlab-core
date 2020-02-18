af75af437e847337836312cf553fdc374bd4032a
Schmeink <Jens.Schmeink@dlr.de>
2020-02-18 13:38:55 +0100

    test of new script to get correct links to compilers

--- tests/build/build-512.bat
+++ tests/build/build-512.bat
@@ -1,15 +1,7 @@
 net use g: \\kpfiler1.intra.dlr.de\AT-KP
 copy features\gitlab_windows_local_settings512.pri local_settings.pri
 
-set path=%path%;%qtdir%\bin;C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Tools\MSVC\14.10.25017\bin\HostX64\x64;C:\Program Files (x86)\Windows Kits\10\bin\x64
-set LIB=%LIB%;C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Tools\MSVC\14.10.25017\lib\x64;C:\Program Files (x86)\Windows Kits\10\lib\10.0.14393.0\ucrt\x64;C:\Program Files (x86)\Windows Kits\10\lib\10.0.14393.0\um\x64;
-set LIBPATH=%LIBPATH%;C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Tools\MSVC\14.10.25017\lib\x64;C:\Program Files (x86)\Windows Kits\10\lib\10.0.14393.0\ucrt\x64;C:\Program Files (x86)\Windows Kits\10\lib\10.0.14393.0\um\x64;
-set INCLUDE=%INCLUDE%;C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Tools\MSVC\14.10.25017\include;C:\Program Files (x86)\Windows Kits\10\include\10.0.14393.0\ucrt;C:\Program Files (x86)\Windows Kits\10\include\10.0.14393.0\shared;C:\Program Files (x86)\Windows Kits\10\include\10.0.14393.0\um;C:\Program Files (x86)\Windows Kits\10\include\10.0.14393.0\winrt;
-
-set LIB=%LIB%;C:\Qt\5.12.6\msvc2017_64\lib
-set LIBPATH=%LIBPATH%;C:\Qt\5.12.6\msvc2017_64\lib
-set INCLUDE=%INCLUDE%;C:\Qt\5.12.6\msvc2017_64\include;C:\Qt\5.12.6\msvc2017_64\include\QtTest
-
 C:\\Qt\\5.12.6\\msvc2017_64\\bin\\qmake.exe gtlab.pro -spec win32-msvc "CONFIG+=debug"
-C:\Qt\Tools\QtCreator\bin\jom.exe -f Makefile qmake_all
+call "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\BuildTools\\VC\\AUXILIARY\\BUILD\\vcvarsall.bat" amd64
+nmake clean
 C:\Qt\Tools\QtCreator\bin\jom.exe
