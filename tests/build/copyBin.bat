266eb1cd692552f0ec5c7fef7f44219af1583c39
Nöthen, Marvin <marvin.noethen@dlr.de>
2020-12-16 13:53:31 +0100

    Resolve "Pipeline Version 1.3.2"

---
+++ tests/build/copyBin.bat
@@ -0,0 +1,32 @@
+@echo off
+
+SET SERVERPATH=C:\deployment\%PLATFORMNAME%\%CATEGORY%\bin\%CI_COMMIT_TAG%
+
+SET BINDIRECTORY=%SERVERPATH%\bin
+SET DEBUGBINDIRECTORY=%SERVERPATH%\binDebug
+
+if not exist "%BINDIRECTORY%" mkdir %BINDIRECTORY%\
+if not exist "%DEBUGBINDIRECTORY%" mkdir %DEBUGBINDIRECTORY%\
+
+if %PLATFORMNAME%==windows (
+
+	copy /y build\GTlab.exe %BINDIRECTORY%\
+	copy /y build\GTlabConsole.exe %BINDIRECTORY%\
+	copy /y build\GTlabGui.dll %BINDIRECTORY%\
+
+	copy /y build\GTlab.exe %DEBUGBINDIRECTORY%\
+	copy /y build\GTlabConsole.exe %DEBUGBINDIRECTORY%\
+	copy /y build\GTlabGui-d.dll %DEBUGBINDIRECTORY%\
+)
+
+if %PLATFORMNAME%==linux (
+
+	copy /y build\GTlab %BINDIRECTORY%\
+	copy /y build\GTlabConsole %BINDIRECTORY%\
+	copy /y build\libGTlabGui.so* %BINDIRECTORY%\
+
+	copy /y build\GTlab %DEBUGBINDIRECTORY%\
+	copy /y build\GTlabConsole %DEBUGBINDIRECTORY%\
+	copy /y build\libGTlabGui-d.so* %DEBUGBINDIRECTORY%\
+)
+
