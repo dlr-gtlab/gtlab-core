de9c532c8c1edbf3f64276325b0dd660e20889ba
Schmeink, Jens <jens.schmeink@dlr.de>
2021-04-21 11:13:22 +0000

    Resolve "Try to test console application with valgrind"

--- tests/build/build-512.bat
+++ tests/build/build-512.bat
@@ -4,4 +4,4 @@ call "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\BuildTools\\VC\\AU
 
 C:\\Qt\\Qt5.12.7\\5.12.7\\msvc2017_64\\bin\\qmake.exe %PROFILENAME% -spec win32-msvc "CONFIG+=%BUILDMODE%"
 nmake clean
-C:\Qt\Qt5.12.7\Tools\QtCreator\bin\jom.exe
+C:\Qt\Qt5.12.7\Tools\QtCreator\bin\jom.exe -j4
