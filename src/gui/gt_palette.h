c51a57b95027345b24bf99fb9d28fae0b2c51923
Jens Schmeink <jens.schmeink@dlr.de>
2022-04-04 11:58:22 +0200

    add comments

--- src/gui/gt_palette.h
+++ src/gui/gt_palette.h
@@ -50,30 +50,88 @@ namespace GtPalette
      */
     GT_GUI_EXPORT QColor basicDark();
 
+    /**
+     * @brief dummyObjectBackground
+     * @return color of objects in the datamodel view dock widget
+     * which are dummy objects
+     */
     GT_GUI_EXPORT QColor dummyObjectBackground();
 
+    /**
+     * @brief newObjectForground
+     * @return foreground color of objects in the datamodel view widget
+     * which are new
+     */
     GT_GUI_EXPORT QColor newObjectForground();
 
+    /**
+     * @brief newObjectForground
+     * @return foreground color of objects in the datamodel view widget
+     * which are changed
+     */
     GT_GUI_EXPORT QColor changedObjectForground();
 
+    /**
+     * @brief footprintWarning
+     * @return color for warnings in the footprint dialog
+     */
     GT_GUI_EXPORT QColor footprintWarning();
 
+    /**
+     * @brief footprintError
+     * @return color for errors in the footprint dialog
+     */
     GT_GUI_EXPORT QColor footprintError();
 
+    /**
+     * @brief infoText
+     * @return color for simple info texts
+     */
     GT_GUI_EXPORT QColor infoText();
 
+    /**
+     * @brief warningText
+     * @return color for warning texts (as in the output dock widget)
+     */
     GT_GUI_EXPORT QColor warningText();
 
+    /**
+     * @brief errorText
+     * @return color for error texts (as in the output dock widget)
+     */
     GT_GUI_EXPORT QColor errorText();
 
+    /**
+     * @brief fatalText
+     * @return color for fatal texts (as in the output dock widget)
+     */
     GT_GUI_EXPORT QColor fatalText();
 
+    /**
+     * @brief fatalTextBackground
+     * @return color for the background of fatal texts
+     * (as in the output dock widget)
+     */
     GT_GUI_EXPORT QColor fatalTextBackground();
 
+    /**
+     * @brief collectionAvailableItemBackground
+     * @return background color for the collection view for
+     * available items
+     */
     GT_GUI_EXPORT QColor collectionAvailableItemBackground();
 
+    /**
+     * @brief collectionInstalledItemBackground
+     * @return background color for the collection view for
+     * installed items
+     */
     GT_GUI_EXPORT QColor collectionInstalledItemBackground();
 
+    /**
+     * @brief environmentModelBack
+     * @return color for the background of the environmental model view
+     */
     GT_GUI_EXPORT QColor environmentModelBack();
 
     }
