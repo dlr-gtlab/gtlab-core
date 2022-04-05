1791b392e4bf8be8dfb3fb0de7d64d50c0d1b026
Martin Siggel <martin.siggel@dlr.de>
2022-04-05 15:37:49 +0200

    Simplified testing code

--- tests/unittests/datamodel/test_gt_enumproperty.cpp
+++ tests/unittests/datamodel/test_gt_enumproperty.cpp
@@ -14,50 +14,36 @@
 
 class TestGtEnumProperty : public ::testing::Test
 {
-
 protected:
-    virtual void SetUp()
-    {
-        m_prop = new GtEnumProperty<EnumContainer::TestEnum>("prop",
-                                                             "test enum",
-                                                             "test brief");
+    GtEnumProperty<EnumContainer::TestEnum> m_prop{
+        "prop", "test enum", "test brief"
+    };
 
-        m_propWithValue = new GtEnumProperty<EnumContainer::TestEnum>("propWValue",
-                                                                      "test enum with value",
-                                                                      "test brief with value",
-                                                                      EnumContainer::TestEnum::B);
-    }
-
-    virtual void TearDown()
-    {
-        delete m_prop;
-        delete m_propWithValue;
-    }
-
-    GtEnumProperty<EnumContainer::TestEnum>* m_prop;
-
-    GtEnumProperty<EnumContainer::TestEnum>* m_propWithValue;
+    GtEnumProperty<EnumContainer::TestEnum> m_propWithValue {
+        "propWValue", "test enum with value", "test brief with value",
+        EnumContainer::TestEnum::B
+    };
 };
 
 TEST_F(TestGtEnumProperty, initialization)
 {
-    ASSERT_EQ(m_prop->getVal(), EnumContainer::TestEnum::A);
-    ASSERT_EQ(m_propWithValue->getVal(), EnumContainer::TestEnum::B);
+    ASSERT_EQ(m_prop.getVal(), EnumContainer::TestEnum::A);
+    ASSERT_EQ(m_propWithValue.getVal(), EnumContainer::TestEnum::B);
 
     // Do not test other init things here since they are already tested in modeProperty test
 }
 
 TEST_F(TestGtEnumProperty, setter)
 {
-    m_prop->setVal(EnumContainer::TestEnum::C);
-    ASSERT_EQ(m_prop->getVal(), EnumContainer::TestEnum::C);
+    m_prop.setVal(EnumContainer::TestEnum::C);
+    ASSERT_EQ(m_prop.getVal(), EnumContainer::TestEnum::C);
 }
 
 TEST_F(TestGtEnumProperty, implicitConversions)
 {
-    m_prop->setVal(EnumContainer::TestEnum::C);
-    ASSERT_EQ(*m_prop, EnumContainer::TestEnum::C);
+    m_prop.setVal(EnumContainer::TestEnum::C);
+    ASSERT_EQ(m_prop, EnumContainer::TestEnum::C);
 
-    *m_prop = EnumContainer::TestEnum::C;
-    ASSERT_EQ(m_prop->getVal(), EnumContainer::TestEnum::C);
+    m_prop = EnumContainer::TestEnum::C;
+    ASSERT_EQ(m_prop.getVal(), EnumContainer::TestEnum::C);
 }
