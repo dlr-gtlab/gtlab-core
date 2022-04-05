dadbc5be2a60bb6bc6e1354781530f2cc9d7335e
Schmeink <Jens.Schmeink@dlr.de>
2022-04-05 09:55:24 +0200

    moved the collection of the superclass based list to a seperate function

--- src/gui/dock_widgets/properties/editors/gt_propertyobjectlinkeditor.h
+++ src/gui/dock_widgets/properties/editors/gt_propertyobjectlinkeditor.h
@@ -74,6 +74,7 @@ private:
      */
     QList<GtObject*> allowedObjects(GtObject* obj);
 
+    QList<GtObject*> allowedSuperClassObjects(GtObject* obj);
 private slots:
     /**
      * @brief selectObjectLink
