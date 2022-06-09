b1e9e9b0624a2f549b6210303610e9bae5642f55
Martin Siggel <martin.siggel@dlr.de>
2022-06-09 13:00:59 +0200

    Change unittest runner

---
+++ tests/unittests/runUnittests.bat
@@ -0,0 +1,7 @@
+REM THIS IS A SCRIPT TO PREPARE AND RUN UNITTESTS
+
+SET PATH=%PATH%;build;lib;lib\%TARGETDIRNAME%;C:\Qt\Qt5.15\5.15.2\msvc2019_64\bin;%DEVTOOLS%\binDebug;
+
+SET PROGRAMPATH="C:\Program Files\OpenCppCoverage"
+
+%PROGRAMPATH%\OpenCppCoverage.exe --sources src --export_type=html --export_type=cobertura --modules lib\%TARGETDIRNAME%\* --excluded_sources build\%UNITTESTSNAME%.exe --excluded_sources %DEVTOOLS%\* -- build\%UNITTESTSNAME%.exe --gtest_output=xml:unittests.xml 
