56fd945f422f3b262ead4f8d22dfcd2e4e6170a1
Martin Siggel <martin.siggel@dlr.de>
2022-11-28 15:21:41 +0100

    Bumped version to Alpha 2

--- src/core/gt_globals.h
+++ src/core/gt_globals.h
@@ -13,8 +13,8 @@
 #define GT_VERSION_MAJOR 2
 #define GT_VERSION_MINOR 0
 #define GT_VERSION_PATCH 0
-#define GT_VERSION_PRE_RELEASE "alpha.1"
-#define GT_VERSION_BUILD "1"
+#define GT_VERSION_PRE_RELEASE "alpha.2"
+#define GT_VERSION_BUILD "0"
 #define GT_MAINTENANCETOOL "MaintenanceTool"
 
 /*
