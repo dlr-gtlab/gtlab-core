21cac540f14e85b23d925e1ca79f957c3d71603c
Bröcker <Marius.Broecker@dlr.de>
2022-12-14 17:04:30 +0100

    updated test modules in gui tests for potential fix

--- tests/guitests/gtlab_core_tests/gtlab_interface_gui_tests/shared/scripts/test_modules.py
+++ tests/guitests/gtlab_core_tests/gtlab_interface_gui_tests/shared/scripts/test_modules.py
@@ -3,7 +3,8 @@ modules = [
     "Test Module Interface",
     "Test Datamodel Interface",
     "Test Mdi Interface",
-    "Test Mdi Interface Ext"
+    "Test Mdi Interface Ext",
+    "Test Process Interface"
 ]
 ''' 
 List of all test modules. This list must be kept up to date and is used to 
