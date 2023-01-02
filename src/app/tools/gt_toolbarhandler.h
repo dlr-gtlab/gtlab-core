844a45ab541a834ecca0ec62274fcd11208b86c8
Reitenbach, Stanislaus <stanislaus.reitenbach@dlr.de>
2023-01-02 15:59:04 +0100

    add some comments

--- src/app/tools/gt_toolbarhandler.h
+++ src/app/tools/gt_toolbarhandler.h
@@ -15,6 +15,9 @@
 
 #include "gt_object.h"
 
+/**
+ * @brief Class for controlling the QML Toolbar.
+ */
 class GtToolbarHandler: public QObject
 {
     Q_OBJECT
@@ -23,24 +26,54 @@ public:
     GtToolbarHandler();
 
 public slots:
+    /**
+     * @brief Called by clicking a qml button
+     * @param btnId button identification string
+     */
     void buttonClicked(const QString& btnId);
 
+    /**
+     * @brief Called on object selection change
+     * @param obj selected object. nullptr if no object is selected
+     */
     void onObjectSelected(GtObject* obj);
 
+    /**
+     * @brief Returns true if current project has an information
+     * file (readme.md) inside the project directory
+     * @return true if current project has an information file. otherwise
+     * false is returned
+     */
     bool projectHasInfo();
 
 private:
+    /// Pointer to selected object
     QPointer<GtObject> m_selectedObj;
 
 signals:
+    /**
+     * @brief Emitted after new project button was clicked.
+     */
     void newProjectButtonClicked();
 
+    /**
+     * @brief Emitted after save project button was clicked.
+     */
     void saveProjectButtonClicked();
 
+    /**
+     * @brief Emitted after open project button was clicked.
+     */
     void openProjectButtonClicked();
 
+    /**
+     * @brief Emitted after undo button was clicked.
+     */
     void undoButtonClicked();
 
+    /**
+     * @brief Emitted after redo button was clicked.
+     */
     void redoButtonClicked();
 
 };
