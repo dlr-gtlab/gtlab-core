bd350540bbcfe7722c8e30e79b831d261c7bfcf6
Martin Siggel <martin.siggel@dlr.de>
2023-03-17 16:11:01 +0100

    Added minor grid lines and fixed dark mode color
    Closes #487

--- src/gui/gt_colors.h
+++ src/gui/gt_colors.h
@@ -162,6 +162,8 @@ GT_GUI_EXPORT QColor randomColor();
 
 GT_GUI_EXPORT QColor gridLine();
 
+GT_GUI_EXPORT QColor gridLineMinor();
+
 GT_GUI_EXPORT QColor gridPoint();
 
 GT_GUI_EXPORT QColor gridAxis();
