c414fc312ae64c761c4307bb00a1fffcd72833bf
Reitenbach, Stanislaus <stanislaus.reitenbach@dlr.de>
2024-05-07 14:34:49 +0200

    add license information

--- src/gui/dialogs/project_settings/gt_projectsettingsgeneraltab.cpp
+++ src/gui/dialogs/project_settings/gt_projectsettingsgeneraltab.cpp
@@ -1,3 +1,14 @@
+/* GTlab - Gas Turbine laboratory
+ *
+ * SPDX-License-Identifier: MPL-2.0+
+ * SPDX-FileCopyrightText: 2023 German Aerospace Center (DLR)
+ * Source File: gt_projectsettingsexternalizationtab.h
+ *
+ *  Created on: 07.05.2022
+ *  Author: Stanislaus Reitenbach (AT-TWK)
+ *  Tel.: +49 2203 601 2907
+ */
+
 #include <QCheckBox>
 #include <QVBoxLayout>
 #include <QSpacerItem>
--- src/gui/dialogs/project_settings/gt_projectsettingsgeneraltab.h
+++ src/gui/dialogs/project_settings/gt_projectsettingsgeneraltab.h
@@ -1,3 +1,13 @@
+/* GTlab - Gas Turbine laboratory
+ *
+ * SPDX-License-Identifier: MPL-2.0+
+ * SPDX-FileCopyrightText: 2023 German Aerospace Center (DLR)
+ * Source File: gt_projectsettingsexternalizationtab.h
+ *
+ *  Created on: 07.05.2022
+ *  Author: Stanislaus Reitenbach (AT-TWK)
+ *  Tel.: +49 2203 601 2907
+ */
 
 #ifndef GTPROJECTSETTINGSGENERALTAB_H
 #define GTPROJECTSETTINGSGENERALTAB_H
