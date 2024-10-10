1bda02909449e85eaf195daabbb8f433e371d251
Schmeink, Jens <Jens.Schmeink@dlr.de>
2024-10-10 09:27:41 +0200

    test implementation of a gt regexp validator

---
+++ src/gui/tools/gt_regexpvalidator.cpp
@@ -0,0 +1,29 @@
+/* GTlab - Gas Turbine laboratory
+ *
+ * SPDX-License-Identifier: MPL-2.0+
+ * SPDX-FileCopyrightText: 2024 German Aerospace Center (DLR)
+ *
+ *  Created on: 10.10.2024
+ *  Author: Jens Schmeink (AT-TWK)
+ *  Tel.: +49 2203 601 2191
+ */
+#include "gt_regexpvalidator.h"
+
+GtRegExpValidator::GtRegExpValidator(QObject* parent) :
+    QValidator(parent)
+{
+
+}
+
+GtRegExpValidator::GtRegExpValidator(const QRegExp& rx, QObject* parent) :
+    QValidator(parent)
+{
+    m_regExp = rx;
+}
+
+QValidator::State
+GtRegExpValidator::validate(QString& input, int& pos) const
+{
+    if (m_regExp.exactMatch(input)) return QValidator::Acceptable;
+    else return QValidator::Intermediate;
+}
---
+++ src/gui/tools/gt_regexpvalidator.h
@@ -0,0 +1,28 @@
+/* GTlab - Gas Turbine laboratory
+ *
+ * SPDX-License-Identifier: MPL-2.0+
+ * SPDX-FileCopyrightText: 2024 German Aerospace Center (DLR)
+ *
+ *  Created on: 10.10.2024
+ *  Author: Jens Schmeink (AT-TWK)
+ *  Tel.: +49 2203 601 2191
+ */
+#ifndef GTREGEXPVALIDATOR_H
+#define GTREGEXPVALIDATOR_H
+
+#include <QValidator>
+
+class GtRegExpValidator : public QValidator
+{
+public:
+    GtRegExpValidator(QObject *parent = nullptr);
+
+    GtRegExpValidator(const QRegExp& rx, QObject* parent = nullptr);
+
+    QValidator::State validate(QString& input, int& pos) const override;
+
+private:
+    QRegExp m_regExp;
+};
+
+#endif // GTREGEXPVALIDATOR_H
