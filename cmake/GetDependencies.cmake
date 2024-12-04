45223a9bd11a277e62570150d4c0187d74f3e6de
Martin Siggel <martin.siggel@dlr.de>
2024-12-04 11:33:46 +0100

    Update GetDependencies.cmake

--- cmake/GetDependencies.cmake
+++ cmake/GetDependencies.cmake
@@ -22,9 +22,7 @@ cmake_policy(SET CMP0097 NEW) # empty submodules list -> don't download these
 if (BUILD_WITH_HDF5 AND GTLAB_USE_BUNDLED_GENH5)
     CPMAddPackage(
       NAME genh5
-      GIT_REPOSITORY "https://github.com/dlr-gtlab/genh5.git"
-      GIT_TAG ${GTLAB_GENH5_VERSION}
-      GIT_SUBMODULES ""
+      URL https://github.com/dlr-gtlab/genh5/archive/refs/tags/${GTLAB_GENH5_VERSION}.zip
     )
 
     set_target_properties(GenH5
@@ -38,9 +36,7 @@ endif()
 if (GTLAB_USE_BUNDLED_LOGGING)
     CPMAddPackage(
       NAME logging
-      GIT_REPOSITORY "https://github.com/dlr-gtlab/gt-logging.git"
-      GIT_TAG ${GTLAB_LOGGING_VERSION}
-      GIT_SUBMODULES ""
+      URL https://github.com/dlr-gtlab/gt-logging/archive/refs/tags/${GTLAB_LOGGING_VERSION}.zip
     )
 
     set_target_properties(GTlabLogging
