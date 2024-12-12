4c5be95d07522a68b3f66ea0835f2cc2af523860
Schmeink, Jens <Jens.Schmeink@dlr.de>
2024-12-12 12:59:44 +0100

    update to improve interface

--- src/dataprocessor/property/gt_colorproperty.h
+++ src/dataprocessor/property/gt_colorproperty.h
@@ -15,16 +15,85 @@
 
 #include "gt_stringproperty.h"
 
+namespace gt
+{
+    // GTlab struct to store rgb color information with the values for
+    // red, green and blue fraction of the color.
+    // The values have a valid range from 0 to 255
+    struct GT_DATAMODEL_EXPORT rgb
+    {
+        rgb(int r, int g, int b);
+        rgb(QString const& hexString);
 
+        // red
+        int m_r;
+        // green
+        int m_g;
+        // blue
+        int m_b;
+
+        // check validity of the values
+        bool valid() const;
+
+        // sets the values of the rgb struct from the information
+        // of a hex string
+        void fromString(QString const& hexCodeInput);
+
+        // write a hex code color from the its values of rgb
+        QString toHexString() const;
+    };
+}
+
+/**
+ * @brief The GtColorProperty class
+ * Property to store color information.
+ * It uses the hex code as string to store color information
+ */
 class GT_DATAMODEL_EXPORT GtColorProperty : public GtStringProperty
 {
     Q_OBJECT
 
 public:
+    /**
+     * @brief GtColorProperty
+     * @param ident - identifier
+     * @param name - name of the property (to show in properties dock)
+     * @param brief - description to give futher information
+     * @param color to initialize. This has to be a hex string.
+     * Easiest way is to define a QColor and call the function QColor::name()
+     */
     GtColorProperty(QString const& ident,
                     QString const& name,
                     QString const& brief,
                     QString const& color);
+
+    /**
+     * @brief GtColorProperty
+     * @param ident - identifier
+     * @param name - name of the property (to show in properties dock)
+     * @param brief - description to give futher information
+     * @param rgb - rgb value struct
+     */
+    GtColorProperty(QString const& ident,
+                    QString const& name,
+                    QString const& brief,
+                    gt::rgb const& rgb);
+
+    /**
+     * @brief GtColorProperty
+     * @param ident - identifier
+     * @param name - name of the property (to show in properties dock)
+     * @param brief - description to give futher information
+     * @param r - red rgb value (between 0 and 255 is valid)
+     * @param g - green rgb value (between 0 and 255 is valid)
+     * @param b - blue rgb value (between 0 and 255 is valid)
+     */
+    GtColorProperty(QString const& ident,
+                    QString const& name,
+                    QString const& brief,
+                    int r, int g, int b);
+
+
 
     /**
      * @brief Overloaded function to convert internal property value to
@@ -47,7 +116,26 @@ public:
     bool setValueFromVariant(QVariant const& val, QString const& unit) override;
 
     GtColorProperty& operator=(const QString& val);
+    GtColorProperty& operator=(const gt::rgb& val);
 
+    /**
+     * @brief toRGB
+     * @return a rgb struct holding the rgb values.
+     * For an invalid property the rgb code is set
+     * to an invalid value (-1, -1, -1)
+     */
+    gt::rgb toRGB() const;
+
+    /**
+     * @brief setFromRGB
+     * Sets property from the values for rgb as three int values of the
+     * gt::rgb struct
+     * @param r - red
+     * @param g - green
+     * @param b - blue
+     */
+    void setFromRGB(int r, int g, int b);
+    void setFromRGB(const gt::rgb& rgb);
 };
 
 
---
+++ tests/unittests/datamodel/test_gt_colorproperty.cpp
@@ -0,0 +1,89 @@
+/* GTlab - Gas Turbine laboratory
+ *
+ * SPDX-License-Identifier: MPL-2.0+
+ * SPDX-FileCopyrightText: 2024 German Aerospace Center (DLR)
+ * Source File: test_gt_colorproperty
+ *
+ *  Created on: 12.12.2024
+ *  Author: Jens Schmeink (AT-TWK)
+ *  Tel.: +49 2203 601 2191
+ */
+
+#include "gtest/gtest.h"
+
+#include "gt_colorproperty.h"
+#include <QColor>
+
+/// This is a test fixture that does a init for each test
+class TestGtColorProperty : public ::testing::Test
+{
+protected:
+    virtual void SetUp()
+    {
+        m_prop = new GtColorProperty("prop", "test color", "test brief",
+                                     QColor(Qt::red).name());
+    }
+
+    virtual void TearDown()
+    {
+        delete m_prop;
+    }
+
+    GtColorProperty* m_prop;
+
+};
+
+TEST_F(TestGtColorProperty, initialization)
+{
+    // default constructor
+    ASSERT_EQ(m_prop->unitCategory(), GtUnit::None);
+    ASSERT_STREQ(m_prop->objectName().toStdString().c_str(),
+                 "test color");
+    ASSERT_STREQ(m_prop->brief().toStdString().c_str(),
+                 "test brief");
+}
+
+TEST_F(TestGtColorProperty, isReadOnly)
+{
+    m_prop->setReadOnly(true);
+    ASSERT_TRUE(m_prop->isReadOnly());
+}
+
+TEST_F(TestGtColorProperty, getter)
+{
+    ASSERT_STREQ(m_prop->get().toStdString().c_str(), "FF0000");
+
+    QColor fromProp = QColor(m_prop->getVal());
+    ASSERT_TRUE(fromProp == QColor(Qt::red));
+}
+
+TEST_F(TestGtColorProperty, setter)
+{
+    m_prop->setVal(QColor(Qt::blue).name());
+    ASSERT_TRUE(m_prop->get() == "#0000FF");
+}
+
+TEST_F(TestGtColorProperty, toRGB)
+{
+    m_prop->setVal(QColor(Qt::blue).name());
+    gt::rgb test1 = m_prop->toRGB();
+    ASSERT_EQ(test1.m_r, 0);
+    ASSERT_EQ(test1.m_g, 0);
+    ASSERT_EQ(test1.m_b, 255);
+
+    m_prop->setVal(QColor(Qt::yellow).name());
+    gt::rgb test2 = m_prop->toRGB();
+    ASSERT_EQ(test2.m_r, 255);
+    ASSERT_EQ(test2.m_g, 255);
+    ASSERT_EQ(test2.m_b, 0);
+}
+
+TEST_F(TestGtColorProperty, setFromRGB)
+{
+    gt::rgb orange(255, 165, 0);
+    std::string orangeHex = "#FFA500";
+
+    m_prop->setFromRGB(orange);
+    ASSERT_STREQ(m_prop->get().toStdString().c_str(), orangeHex.c_str());
+}
+
