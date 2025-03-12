13411351092d4d0c33b72e8c0164038f341d6440
Schmeink, Jens <Jens.Schmeink@dlr.de>
2025-03-12 09:16:18 +0100

    add additional test

--- tests/unittests/datamodel/test_gtregexp.cpp
+++ tests/unittests/datamodel/test_gtregexp.cpp
@@ -48,3 +48,41 @@ TEST(RegExp, restrictRegExpWithObjectNames)
     EXPECT_FALSE(match(re, "Hello"));
     EXPECT_FALSE(match(re, "World"));
 }
+
+TEST(RegExp, restrictRegExpWithObjectSiblingsNames)
+{
+    auto re = gt::re::onlyLetters();
+
+    auto* parent = new GtObject;
+    auto* c1 = new GtObject;
+    c1->setObjectName("Hello");
+    parent->appendChild(c1);
+
+    auto* c2 = new GtObject;
+    c2->setObjectName("World");
+    parent->appendChild(c2);
+
+    auto* c3 = new GtObject;
+    c3->setObjectName("Foo");
+    parent->appendChild(c3);
+
+    gt::re::restrictRegExpWithObjectSiblingsNames<GtObject>(*c3, re);
+
+    auto match = [](const auto& re, const auto& string) {
+        return re.indexIn(string) >= 0;
+    };
+
+    /// Other names should still be possible
+    EXPECT_TRUE(match(re, "Python"));
+    EXPECT_TRUE(match(re, "HiEverybody"));
+
+    /// The existing name should still be possible
+    EXPECT_TRUE(match(re, "Foo"));
+
+    /// The name of the siblings should not be allowed
+    EXPECT_FALSE(match(re, "Hello"));
+    EXPECT_FALSE(match(re, "World"));
+
+    delete parent;
+}
+
